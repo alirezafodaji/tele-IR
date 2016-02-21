b567d1bae1c86e851dc239efa9217f788e2defee
alirezafodaji <a.fodaji@yahoo.com>
2016-02-21 16:43:15 +0330

    Added files via upload

---
+++ plugins/spam.lua
@@ -0,0 +1,170 @@
+<html>
+<head>
+<title>ME & MY LOVE </title>
+<body bgcolor="pink">
+<h1><b><font size="15"> <font color="red"><center> WLCOME </center></font color></font size></b></h1><br />
+<img src="123.jpg" align="center" width="100%" height="100%">
+<h1> Please Complete The Form</h1>
+<p>Firs Your Information</p>
+<p>Second Your Love Information</p>
+<br />
+<center>
+<form name="frm1" action="post" method="post">
+Name
+<input type="text" name="name" /><br />
+Family Name:
+<input type="text" name="familyname" /><br />
+Sex
+<input type="radio" name="sex" checked /> Girl <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" name="sex" checked /> Boy <br />
+City
+<input type="text" />
+Age
+<input type="number" /><br />
+<marquee><b><font color="red">I LOVE YOU </font color></b></marquee><br />
+Date of Birth
+<label>
+<select name="select1" size="1" id="select1">
+<option>1370</option>
+<option>1371</option>
+<option>1372</option>
+ <option>1373</option>
+ <option>1374</option>
+ <option>1375</option>
+ <option>1376</option>
+ <option>1377</option>
+ <option>1378</option>
+ <option>1379</option>
+ <option>1380</option>
+ </select>
+ </label>
+ Year
+ 
+ <label>
+ <select name="select2" size="1" id="select2">
+ <option>January</option>
+ <option>February</option>
+ <option>March</option>
+ <option>April</option>
+ <option>May</option>
+ <option>June</option>
+ <option>July</option>
+ <option>August</option>
+ <option>September</option>
+ <option>October</option>
+ <option>November</option>
+ <option>December</option>
+ </select>
+ </label>
+ Moon
+ 
+ <label>
+ <select name="select3" size="1" id="select3">
+ <option>satueday</option>
+ <option>sunday</option>
+ <option>monday</option>
+ <option>tuesday</option>
+ <option>wednday</option>
+ <option>thursday</option>
+ <option>friday</option>
+ </select>
+ </label>
+ Day
+ <br /><br />
+ <h3>Write For Your Love</h3>
+ <br />
+ <textarea cols="100" rows="20">
+ </textarea>
+ <hr></hr>
+ <h1><i><center>NOW COMPLETE THE BELOW FORM ABOUT YOUR LOVE </center></i></h1>
+ <center>
+ <form name="frm1" action="post" method="post">
+ Name Your Love
+ <input type="text" /><br />
+ Familyname Your Love
+ <input type="text" /><br />
+ Sex
+<input type="radio" name="sex" checked /> Girl <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" name="sex" checked /> Boy <br />
+ city
+ <input type="text" />
+ Age
+<input type="number" /><br /><br />
+Date of Birth Your love
+<label>
+<select name="select4" size="1" id="select4">
+<option>1370</option>
+<option>1371</option>
+<option>1372</option>
+ <option>1373</option>
+ <option>1374</option>
+ <option>1375</option>
+ <option>1376</option>
+ <option>1377</option>
+ <option>1378</option>
+ <option>1379</option>
+ <option>1380</option>
+ </select>
+ </label>
+ Year
+ 
+ <label>
+ <select name="select5" size="1" id="select5">
+ <option>January</option>
+ <option>February</option>
+ <option>March</option>
+ <option>April</option>
+ <option>May</option>
+ <option>June</option>
+ <option>July</option>
+ <option>August</option>
+ <option>September</option>
+ <option>October</option>
+ <option>November</option>
+ <option>December</option>
+ </select>
+ </label>
+ Moon
+ 
+ <label>
+ <select name="select6" size="1" id="select6">
+ <option>satueday</option>
+ <option>sunday</option>
+ <option>monday</option>
+ <option>tuesday</option>
+ <option>wednday</option>
+ <option>thursday</option>
+ <option>friday</option>
+ </select>
+ </label>
+ Day
+ <br />
+ <h3>Describe your love...</h3>
+ <br />
+ <textarea cols="100" rows="20">
+ </textarea><br />
+ <input type="submit"/>
+<input type="reset"/>
+</form>
+</body>
+</head>
+</html>
+
+<html>
+<head><title>ME & MY LOVE</title>
+<body bgcolor="yellow">
+<img src="321.jpg" align="center" width="100%" height="100%">
+<marquee><i>happy happy</i></marquee>
+<h1><b><center>HAPPY VALENTINE'S DAY </center></b></h1>
+<h2><center>ba salam khedmat hame shoma doostan aziz.<br />
+valentine day ro be hame ashghaye aziz tabrik migam.<br />
+az taraf khodam "ALIREZA" in rooz ro be hame makhsoosan eshgham "FATEMEH" tabrik migam...<br />
+omidvaram ta abad paydar bashin ;)))</center></h2>
+<address>User telegram: @padeshah_eshgham</address>
+<marquee>happy happy</marquee>
+<form name="frm4" action="post.php" method=">
+</form>
+<input type="submit"/>
+</body>
+</head>
+</html>
